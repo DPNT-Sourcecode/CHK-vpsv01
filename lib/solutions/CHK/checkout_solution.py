b556af8c512a2d928dad07ef087b33933369c990
sbx_user1051 <sbx_user1051@169.254.133.169>
2022-03-24 18:57:57 +0000

    Thu Mar 24 18:57:57 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    c = 0
-    for sku in "ABCD":
-        c += sku.count("A")
+    base_prices = {"A" : 50, "B" : 30, "C" : 20, "D" : 15 }
+    discounts = {"A" : (3,20), "B" : (2,15)}  # if there are 3 A, discount is 20 gbp
+
+    skus = skus.upper()
+    quantities = {item : skus.count(item) for item in base_prices.keys()}
+    checkout = sum(quantities[product]*price for product,price in base_prices.items())
+    for product,t in discounts.items():
+        n_for_discount, discount = t
+        checkout -= (quantities[product] // n_for_discount) * discount
+
